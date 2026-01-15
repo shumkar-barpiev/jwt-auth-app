6c5c35c661b15de83e2b91f60a95c409d534b2ed
shumkar-barpiev <barpievsh@gmail.com>
2026-01-15 10:17:19 +0100

    feat: create mail sender service

---
+++ src/main/java/com/shumkar/jwt_auth/service/EmailService.java
@@ -0,0 +1,25 @@
+package com.shumkar.jwt_auth.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendVerificationEmail(String to, String subject, String textBody) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(textBody, true);
+
+        mailSender.send(message);
+    }
+}
