3c2af385b8e6ff0cf1ff5514629a2aaaf89f02f1
shumkar-barpiev <barpievsh@gmail.com>
2026-01-15 10:16:36 +0100

    feat: create dto files

---
+++ src/main/java/com/shumkar/jwt_auth/dto/RegisterUserDto.java
@@ -0,0 +1,14 @@
+package com.shumkar.jwt_auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String username;
+    private String email;
+    private String password;
+
+
+}
